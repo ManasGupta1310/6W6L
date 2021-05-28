da30c6a2b03b02e4f6330067b00040c91d466515
ManasGupta <mmanasggupta@gmail.com>
2021-05-28 22:52:53 +0530

    First script

---
+++ concat.py
@@ -0,0 +1,14 @@
+import argparse
+
+parser=argparse.ArgumentParser(description="Read the content of a file")
+parser.add_argument("cat", metavar="concat")
+parser.add_argument("Filename", type=str, metavar="Name", action="store")
+args=parser.parse_args()
+
+f=open(args.Filename)
+
+if args.cat:
+    if args.Filename:
+        for line in f:
+            print(line, end="")
+        print()
