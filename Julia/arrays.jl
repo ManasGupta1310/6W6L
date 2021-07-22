4fd514d6fc44380df96336a46bdce28315b6fe2c
ManasGupta <mmanasggupta@gmail.com>
2021-07-22 23:40:29 +0530

    New files

---
+++ Julia/arrays.jl
@@ -0,0 +1,8 @@
+arr=[1,2,3,4,5] #col matrix
+vector1=[1 2 3 4 5] #row matrix
+two_d=[1 2 3 4 5;6 7 8 9 0] #2d array
+array=rand(5) #array of five random values
+array2=rand(5,5) #2-d array of 25 random values
+println(two_d[2,2])
+println(arr[2])
+println(vector1[2])
